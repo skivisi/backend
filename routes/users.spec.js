227c9b7dfea410dc754af65b0ddfa68b4446bc88
mhatano <masaki.hatano@rakus-partners.co.jp>
2023-07-06 09:38:40 +0900

    _

--- routes/users.spec.js
+++ routes/users.spec.js
@@ -1,42 +1,31 @@
+
 const express = require("express");
 const app = express();
-const request = require('supertest')
-const { PrismaClient } = require("@prisma/client");
+const request = require('supertest');
 
-// モックデータ
-const mockUser = {
-  userId: 1,
-  // その他のユーザーのプロパティ
-};
-
-let prisma;
-
-beforeAll(() => {
-  // Prismaのモックを作成
-  prisma = new PrismaClient();
-  prisma.user.findUnique = jest.fn().mockResolvedValue(mockUser);
-
-  // グローバルオブジェクトにPrismaをセット
-  global.prisma = prisma;
-
-  // あなたのルートハンドラ
-  const handler = require('./users'); // ルートハンドラの実際のパスに置き換えてください
-
-  app.use(express.json());
-  app.use("/user", handler); // Prismaのモックをハンドラに渡します
+// jest.mock("@prisma/client"); // 追加: PrismaClientのモックを使用する
+jest.mock("@prisma/client", () => {
+  return {
+    PrismaClient: jest.fn().mockImplementation(() => {
+      return {
+        user: {
+          findUnique: jest.fn().mockResolvedValue({ userId: 1, name: "Alice" }),
+        },
+      };
+    }),
+  };
 });
 
-afterAll(() => {
-  // グローバルオブジェクトをクリーンアップ
-  delete global.prisma;
-});
+const handler = require('./users'); // ルートハンドラの実際のパスに置き換えてください
+app.use(express.json());
+app.use("/users", handler); // Prismaのモックをハンドラに渡します
 
 describe('GET /user', () => {
   it('responds with 200 status code', async () => {
-    const response = await request(app).get('/user?userId=1');
-    console.log(response.body)
+    const response = await request(app).get('/users?userId=1');
+    console.log(response.body);
     expect(response.statusCode).toBe(200);
     // 必要に応じてモックデータの検証を追加
-    expect(response.body.userId).toBe(mockUser.userId);
+    expect(response.body.userId).toBe(1); // 追加: userIdの値を検証
   });
 });
